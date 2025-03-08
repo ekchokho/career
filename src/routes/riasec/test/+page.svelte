a130ed2b1a64e1b934525dc158c0b8d54846c7ae
Gobinda Gorai <87747093+ekchokho@users.noreply.github.com>
2025-03-08 10:25:22 +0530

    Add files via upload

---
+++ src/routes/riasec/test/+page.svelte
@@ -0,0 +1,48 @@
+<script>
+  import { _ } from 'svelte-i18n';
+  import { onMount } from 'svelte';
+  import Riasec from '$lib/filter/RIASEC.svelte';
+  import {studentGender, studentClass } from "../../../stores";
+
+  onMount(() => {
+    const gc_path = '/riasec/test-' + $studentGender + '-' + $studentClass;
+    console.log(gc_path);
+    window.goatcounter.count({
+        path: gc_path,
+    })
+
+    // fetch('http://wbcareerportal.goatcounter.com/api/v0/count', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     'Authorization': 'Bearer 7yj4tgzqh3z3dtvpjfq9rifs2ndcjmxugu64b994b2wy002l'
+    //   },
+    //   body: JSON.stringify({
+    //     'no_sessions': true,
+    //     'hits': [
+    //       {
+    //         'path': '/riasec/test-' + {$studentGender} + '-' + {$studentClass},
+    //       },
+    //     ]
+    //   })
+    // });
+  });
+</script>
+
+<svelte:head>
+  <title>RIASEC - Career Guidance Portal</title>
+  <meta name="description" content="Filter your career choices based on your interests!" />
+</svelte:head>
+
+<section class="">
+  <div class="col mt-5">
+    <h1>{$_('riasec_page_title')}</h1>
+  </div>
+
+  <section id="riasec">
+    <Riasec />
+  </section>
+</section>
+
+<style>
+</style>
